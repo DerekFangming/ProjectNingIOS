5610711de4d6b3a0ff61f66499c2e0bf48415584
DerekFangming <synfm@126.com>
2016-02-12 09:28:03 -0500

    Init with empty one page app

---
+++ ProjectNingIOS/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  ProjectNingIOS
+//
+//  Created by NingFangming on 2/12/16.
+//  Copyright © 2016 fangming. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
