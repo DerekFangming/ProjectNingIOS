3614da33444b8cd5b50273fa566796452df92b23
DerekFangming <synfm@126.com>
2016-02-27 08:22:13 -0500

    Test xcode commit

--- ProjectNingIOS/PNUser.h
+++ ProjectNingIOS/PNUser.h
@@ -24,14 +24,39 @@
 + (instancetype)currentUser;
 
 
+/**
+    Method to register a user to project ning
+    In the call back block, either PNuser or NSError is nil. Certain action can then start after checking the result of the register.
+    If there is an error, the error message can be accessed by calling [NSError localizedDescription]
+    After the login, current user will have three attributs : username, expDate and emailConfirmed
+    @param loginUsername the username of the user
+    @param loginPassword the password of the user
+ */
 + (void) registerUserWithUsername:(NSString *)loginUsername
-                     andPassword:(NSString *)loginPassword
-                        response:(void (^)(PNUser *, NSError *))response;
+                      andPassword:(NSString *)loginPassword
+                         response:(void (^)(PNUser *, NSError *))response;
+/**
+    Method to log in a user by username and password
+    @param loginUsername the username of the user
+    @param loginPassword the password of the user
+ */
++ (void) loginUserWithUsername:(NSString *)loginUsername
+                   andPassword:(NSString *)loginPassword
+                      response:(void (^)(PNUser *, NSError *))response;
 
+/**
+    Method to retrieve the username of a user object
+ */
 - (NSString *) username;
 
+/**
+ Method to retrieve the session expiration date of a user object
+ */
 - (NSDate *) expDate;
 
+/**
+ Method to check if the email of the user is confirmed
+ */
 - (BOOL) emailConfirmed;
 
 @end
