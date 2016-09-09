b0f5a2683dd5f1f169d7e9f444d6b7742ec1373f
DerekFangming <synfm@126.com>
2016-09-09 15:14:11 -0400

    Update comments for new methods

--- ProjectNingIOS/PNImage.h
+++ ProjectNingIOS/PNImage.h
@@ -7,7 +7,7 @@
 //
 
 #import <Foundation/Foundation.h>
-#import <UIKit/UIKit.h> 
+#import <UIKit/UIKit.h>
 #import "PNUser.h"
 
 @interface PNImage : NSObject
@@ -15,29 +15,39 @@
 + (instancetype)imageManager;
 
 /**
-    Upload an image to server
-    If the response error is not nil, there is error in the upload process
-    @param img the image in UIImage form
-    @param type the type of the image. This will be validated by the server and may be changed
+ Upload an image to server
+ If the response error is not nil, there is error in the upload process
+ @param img the image in UIImage form
+ @param type the type of the image. This will be validated by the server and may be changed
  */
 + (void) uploadImage:(UIImage *) img
               inType:(NSString *) type
             response:(void (^)(NSError *))response;
 
 /**
-    Upload an image to server with a customized title
-    If the response error is not nil, there is error in the upload process
-    @param img the image in UIImage form
-    @param type the type of the image. This will be validated by the server and may be changed
+ Upload an image to server with a customized title
+ If the response error is not nil, there is error in the upload process
+ @param img the image in UIImage form
+ @param type the type of the image. This will be validated by the server and may be changed
  */
 + (void) uploadImage:(UIImage *) img
               inType:(NSString *) type
            withTitle:(NSString *) title
             response:(void (^)(NSError *))response;
 
+/**
+	Delete an image on server.
+	Note that you need to be the creator of that image to delete it.
+	@param imageId the id of the image to be deleted
+ */
 + (void) deleteImage:(NSNumber *) imageId
             response:(void (^)(NSError *))response;
 
+/**
+	Get a list of IDs for images with the specific type.
+	Note that only images created by you with that type will get retrieved
+	@param type the type name of the images
+ */
 +(void) getImageIdListByType:(NSString *) type
                     response:(void (^)(NSMutableArray *list, NSError *error))response;
 @end
