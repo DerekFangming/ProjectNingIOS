308a372264f2fc14326ffe4f55765463172e6a04
DerekFangming <synfm@126.com>
2016-09-10 10:33:45 -0400

    Create obj PNStranger to hold data for each stranger

---
+++ ProjectNingIOS/PNStranger.h
@@ -0,0 +1,13 @@
+//
+//  PNStranger.h
+//  ProjectNingIOS
+//
+//  Created by NingFangming on 9/9/16.
+//  Copyright © 2016 fangming. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface PNStranger : NSObject
+
+@end
---
+++ ProjectNingIOS/PNStranger.m
@@ -0,0 +1,13 @@
+//
+//  PNStranger.m
+//  ProjectNingIOS
+//
+//  Created by NingFangming on 9/9/16.
+//  Copyright © 2016 fangming. All rights reserved.
+//
+
+#import "PNStranger.h"
+
+@implementation PNStranger
+
+@end
