669d17738af13de097cf89a709faf5a0978866c1
DerekFangming <synfm@126.com>
2016-09-20 12:23:40 -0400

    Update document for PNStranger

--- ProjectNingIOS/PNStranger.m
+++ ProjectNingIOS/PNStranger.m
@@ -10,6 +10,11 @@
 
 @implementation PNStranger
 
+/**
+    Init method for PNStranger.
+    @param newAvatar the avatar image of the stranger
+    @param newUserId the internal user id of the stranger
+ */
 - (id)initWithAvatar:(UIImage *) newAvatar andUserId:(NSNumber *) newUserId{
     self = [super init];
     
@@ -21,10 +26,16 @@
     return self;
 }
 
+/**
+ Method to retrieve the avatar of a stranger object
+ */
 - (UIImage *) avatar{
     return self->avatar;
 }
 
+/**
+ Method to retrieve the user id of a stranger object
+ */
 - (NSNumber *) userId{
     return self->userId;
 }
