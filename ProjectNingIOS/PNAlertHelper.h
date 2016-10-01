71cfcb8d5eeb5e4b97fc2c332201adba86a20fff
DerekFangming <synfm@126.com>
2016-10-01 00:08:36 -0400

    Update documentation for methods

--- ProjectNingIOS/PNAlertHelper.h
+++ ProjectNingIOS/PNAlertHelper.h
@@ -12,14 +12,31 @@
 @interface UIAlertController (Helpers)
 
 /**
+    Show error message with an error message. The title is fixed
+    @param message the error message
+    @param controller the controller to display the alert
  */
 +(void)showErrorAlertWithErrorMessage:(NSString *)message
                                  from:(UIViewController *)controller;
 
+/**
+    Show alert with customized title and error message
+    @param title the title of the alert
+    @param message the error message
+    @param controller the controller to display the alert
+ */
 +(void)showAlertWithTitle:(NSString *)title
                andMessage:(NSString *)message
                      from:(UIViewController*)controller;
 
+/**
+    Show alert dialog with customized title, message and button controls
+    @param title the title of the alert
+    @param message the error message
+    @param controller the controller to display the alert
+    @param burronTitles an array of strings containing button texts
+    @param handler the complete handler block
+ */
 +(void)showDialogWithTitle:(NSString *)title
                 andMessage:(NSString *)message
                       from:(UIViewController *)controller
